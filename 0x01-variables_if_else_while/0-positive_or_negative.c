db3db78c42f0617c4376ccc9d87200087075ab35
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-06-20 09:41:32 +0200

    mandotory task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,17 +21,17 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else
 	{
 		if (n < 0)
 		{
-			printf("%d is negetive\n");
+			printf("%d is negetive\n", n);
 		}
 		else
 		{
-			printf("%d is zero\n");
+			printf("%d is zero\n", n);
 		}
 	}
 	return (0);
