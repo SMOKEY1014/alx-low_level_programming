bd0e850c9dea2c3df455fd4bf64d10b95322fe91
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-06-20 11:04:28 +0200

    Advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int n, m;
+
 	for (n = 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
@@ -15,7 +16,7 @@ int main(void)
 			{
 				putchar(n);
 				putchar(m);
-				if(n != 56 || m != 57)
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
@@ -24,5 +25,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
