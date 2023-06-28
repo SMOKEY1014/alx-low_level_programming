6e1def47c3853a5545b9c828b791615253cc5b83
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-06-28 12:38:43 +0200

    betty fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,32 +2,29 @@
 
 /**
 * char *_strcat - a function that appends the src string to the dest string
-* including the overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte.
+* including the overwriting the terminating null byte (\0) at the end of dest, 
+* and then adds a terminating null byte.
 * *dest_ptr is a pointer.
 * @dest: appends to
 * @src: Appends from
 * Return: pointer to the resulting string
 */
 
-char* _strcat(char* dest, const char* src) {
-    char* dest_ptr = dest;
-    
-    
+char* _strcat(char* dest, const char* src) 
+{
+	char* dest_ptr = dest;
+
 	while (*dest_ptr != '\0') 
 	{
 		dest_ptr++;
 	}
-    
-    // Append each character from src to dest
+
 	while (*src != '\0') 
 	{
 		*dest_ptr = *src;
 		dest_ptr++;
 		src++;
 	}
-    
-    // Add the terminating null byte
 	*dest_ptr = '\0';
-    
-	return dest;
+	return (dest);
 }
