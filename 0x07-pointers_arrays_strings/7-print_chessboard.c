3516f3770ed6419d6a7e91fa70e1015ffb4acc22
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-03 15:15:42 +0200

    check

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,7 +12,6 @@
 
 void print_chessboard(char (*a)[8])
 {
-<<<<<<< HEAD
 	int x;
 	int y;
 
@@ -21,20 +20,7 @@ void print_chessboard(char (*a)[8])
 		for (y = 0; y < 8; y++)
 		{
 			_putchar(a[x][y]);
-			_putchar('\n');
 		}
-	}
-}
-=======
-	int i;
-	int j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
 		_putchar('\n');
 	}
 }
-
->>>>>>> 2f3ed62d3d076e68862483f08c3b42a873f86738
