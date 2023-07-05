ae1d5d145cbf4ef2128a86af8fe2c2fb339ce2b7
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-05 14:48:09 +0200

    Tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of the string.
+ * @s: The string to be measured with.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s)
+	{
+		l++;
+		l += _strlen_recursion(s + 1);
+	}
+
+	return (l);
+}
