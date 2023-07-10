d8cd75c5976def4d2265a66c4b147f8a309311dd
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-10 12:40:20 +0200

    are we done yet?

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+
+for file in $c_files; do
+	gcc -c "$file" -o "${file%.c}.o"
+done
+
+ar rcs liball.a *.o
