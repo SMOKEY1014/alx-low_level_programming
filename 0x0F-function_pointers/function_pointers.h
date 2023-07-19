b0b81d7ba767f743d84a5aa8220b5dcd2216a248
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-19 14:19:31 +0200

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 
 int int_index(int *array, int size, int (*cmp)(int));
-void array_iterator(int *array, size_t size, void (*action)(int));void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 
 #endif
