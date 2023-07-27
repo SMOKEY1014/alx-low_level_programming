56f1f97c3425641a34a28b52202d2c8287727dca
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-27 10:53:19 +0200

    betty fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,17 +13,17 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *temp = *head;
-	unsigned int length = 0;
+	unsigned int len = 0;
 
-	while (str[length])
-		length++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->length = length;
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
