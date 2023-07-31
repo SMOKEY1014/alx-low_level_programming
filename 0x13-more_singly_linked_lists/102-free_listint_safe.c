954be7b7ada62973389c46b5ae168bf31ffe7f99
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-07-31 16:50:48 +0200

    advanced

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,68 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - Function for freeing a listint_t linked list.
+ * @h: (**)ptr to ptr to the head of the list.
+ *
+ * Return: returns size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *s, *f, *tempo;
+	size_t cnt = 0;
+
+	if (h == NULL || *h == NULL)
+	{
+		return (0);
+	}
+
+	s = *h;
+	f = *h;
+
+	while (s != NULL && f != NULL && f->next != NULL)
+	{
+		s = s->next;
+		f = f->next->next;
+
+		if (s == f)
+		{
+			s = *h;
+
+			while (s != f)
+			{
+				tempo = f;
+				f = f->next;
+				free(tempo);
+				cnt++;
+			}
+
+			while (f->next != s)
+			{
+				tempo = f;
+				f = f->next;
+				free(tempo);
+				cnt++;
+			}
+
+			tempo = f;
+			f = f->next;
+			free(tempo);
+			cnt++;
+
+			*h = NULL;
+			return (cnt);
+		}
+	}
+
+	while (*h != NULL)
+	{
+		tempo = *h;
+		*h = (*h)->next;
+		free(tempo);
+		cnt++;
+	}
+
+	return (cnt);
+}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -23,8 +23,9 @@ void free_listint2(listint_t **head);
 void free_listint(listint_t *head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *add_nodeint(listint_t **head, const int n);
-
-
+size_t free_listint_safe(listint_t **h);
+size_t print_listint_safe(const listint_t *head);
+listint_t *reverse_listint(listint_t **head);
 
 
 #endif
