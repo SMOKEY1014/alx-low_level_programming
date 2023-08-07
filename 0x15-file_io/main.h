10f75f83f5f78036dfc3bcd39da8ee8ab1cdb7b6
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-08-07 16:22:49 +0200

    message

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,10 +7,12 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+
 
 #endif
