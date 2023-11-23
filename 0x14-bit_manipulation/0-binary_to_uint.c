189731582c1ba2e503ccf978a864df0ab7b3a555
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-11-23 08:53:46 +0200

    task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  *binary_to_uint - a program that converts a binary number to an
+ *   *unsigned integer
+ *    *@b: binary
+ *     *Return: integer unsigned
+ *      */
+unsigned int binary_to_uint(const char *b)
+{
+	int x;
+	unsigned int y;
+
+	y = 0;
+	if (!b)
+		return (0);
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] != '0' && b[x] != '1')
+			return (0);
+	}
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		y <<= 1;
+		if (b[x] == '1')
+			y += 1;
+	}
+	return (y);
+}
