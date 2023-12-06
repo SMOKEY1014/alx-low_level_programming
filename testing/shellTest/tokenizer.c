d1ed43c19724b82d418f657499a8270c6677e002
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-12-06 08:17:40 +0200

    k

--- testing/shellTest/tokenizer.c
+++ testing/shellTest/tokenizer.c
@@ -1,61 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_ARGUMENTS 10
 
-int main(void)
-{
-    char input[] = "ls -l";
+void executeCommand(char *input) {
+    // Tokenize the input string
+    char *delimiters = " \t\r\n\a";
+    char *args[MAX_ARGUMENTS];
+    int num_args = 0;
 
-    char *delimiters = " ;:";
-    char *commands[MAX_ARGUMENTS];
-    int num_commands = 0;
-
-    /* Tokenize the input string based on semicolons */
     char *token = strtok(input, delimiters);
-
-    while (token != NULL && num_commands < MAX_ARGUMENTS)
-    {
-        commands[num_commands] = token;
-        num_commands++;
-
+    while (token != NULL && num_args < MAX_ARGUMENTS) {
+        args[num_args] = token;
+        num_args++;
         token = strtok(NULL, delimiters);
-
-        
     }
+    args[num_args] = NULL;
 
-    for(int b = 0; b < num_commands; b++)
-        {
-            printf("Arg %d : %s \n", b, commands[b]);
-        }
+    // Execute the program
+    char *programPath = args[0]; // The first token is assumed to be the program path
+    char *envp[] = { NULL };
 
-    // /* Process each command separately */
-    // for (int i = 0; i < num_commands; i++)
-    // {
-    //     char *args[MAX_ARGUMENTS];
-    //     int num_args = 0;
+    if (execve(programPath, args, envp) == -1) {
+        perror("execve");
+        exit(EXIT_FAILURE);
+    }
+}
 
-    //     /* Tokenize each command based on spaces */
-    //     token = strtok(commands[i], " ");
+int main(void) {
+    char input[] = "/bin/ls -l";  // Example input string
 
-    //     while (token != NULL && num_args < MAX_ARGUMENTS)
-    //     {
-    //         args[num_args] = token;
-    //         num_args++;
+    // Execute the command
+    executeCommand(input);
 
-    //         token = strtok(NULL, " ");
-    //     }
-
-    //     /* Do something with the arguments (e.g., execute the command) */
-    //     printf("Command %d:\n", i + 1);
-    //     for (int j = 0; j < num_args; j++)
-    //     {
-    //         printf("  Arg %d: %s\n", j + 1, args[j]);
-    //     }
-    //     printf("\n");
-    // }
+    // This code will not be reached if execve is successful
+    printf("This will not be printed.\n");
 
     return 0;
 }
-
